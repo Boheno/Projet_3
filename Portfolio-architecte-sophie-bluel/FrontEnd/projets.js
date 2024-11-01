f17395a176f6740383a7c3f77b4ff405034ea08c
Nolwenn <bohelay.nolwenn@gmail.com>
2024-11-01 11:52:23 +0100

    build (category): fonction = création nouveau tableau catégorie, 3

--- Portfolio-architecte-sophie-bluel/FrontEnd/projets.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/projets.js
@@ -1,13 +1,44 @@
 const works = await fetch("http://localhost:5678/api/works")
-  .then(works => works.json())
+  .then(works => 
+    works.json())
+
+  // Fonction génération des filtres catégories
+
+function generateCategory(works) {
+   //Nouveau tableau des catégories
+    let worksCategory = []
+    works.forEach(projet => {
+      worksCategory.push(projet["categoryId"])
+    })
+    let worksCategoryNew =[...new Set (worksCategory)];
+  console.log(worksCategoryNew)
+
+  for (let i=0; i<works.length; i++){
+    let categoryElements = document.createElement ("div");
+    let categoryButton = document.createElement ("button");
+    
+//Récupération du tableau Category
+    
+    //Listener du bouton
+
+//Integration HTML
+    categoryButton.innerText = works[i].categoryId;
+  
+    categoryElements.appendChild (categoryButton);
+
+    document.querySelectorAll(".gallery").innerHTML=categoryElements;
+    document.querySelector(".gallery").appendChild (categoryElements);
+  }
+}
+generateCategory (works);
 
   // Fonction génération des projets 
 
 function generateWorks(works){
   for (let i=0; i<works.length; i++){
-    const worksElement = document.createElement ("figure");
-    const imageWorks = document.createElement ("img");
-    const captionWorks = document.createElement ("figcaption");
+    let worksElement = document.createElement ("figure");
+    let imageWorks = document.createElement ("img");
+    let captionWorks = document.createElement ("figcaption");
 
     imageWorks.src = works[i].imageUrl;
     captionWorks.innerText = works[i].title;
@@ -21,21 +52,3 @@ function generateWorks(works){
 }
 generateWorks (works);
 
-// Fonction génération des filtres catégories
-
-function generateCategory(works){
-  for (let i=0; i<works.length; i++){
-    const categoryElements = document.createElement ("div");
-    const categoryButton = document.createElement ("button");
-
-    //Listener du bouton
-
-    categoryButton.innerText = works [i].name;
-
-    categoryElements.appendChild (categoryButton);
-
-    document.querySelectorAll(".gallery").innerHTML=categoryElements;
-    document.querySelector(".gallery").appendChild (categoryElements);
-  }
-}
-generateCategory (works);
