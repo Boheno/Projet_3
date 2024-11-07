2cc469edfcadc8b57d7f348c8d0df9982e34c8a3
Nolwenn <bohelay.nolwenn@gmail.com>
2024-11-07 11:18:28 +0100

    build (login): début fonction pour se connecter + modif style

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/login.js
@@ -0,0 +1,33 @@
+function seConnecter(){
+    let nomUtilisateur = document.getElementById("userEmail").value;
+    let motDePasse = document.getElementById("password").value;
+
+    let donnees = {
+        userEmail: nomUtilisateur,
+        password: motDePasse
+    };
+
+const login = await fetch("http://localhost:5678/api/users/login",{ 
+    method:"post",
+    headers:{"Content-type":"application/json"},
+    body: {JSON.stringify(donnees)}
+})
+.then(response => response.json())
+.then(data => {
+    if (data.success){
+        console.log("bravo");//Renvoyer vers la page d'accueil
+    } else {
+        console.log("nope");//indiquer Email ou mdp incorrect
+    }
+ })
+}
+// + sauvegarde du token pour l'ajout ou supp de projets
+
+//Fonction de listenner du bouton se connecter, importer via html le bouton, submit avec event.preventDefault()
+function btnConnecter (){
+    let btnSeConnecter = document.querySelector(".btn-connection");
+
+    btnSeConnecter.onclick = () => {
+        
+    }
+}
