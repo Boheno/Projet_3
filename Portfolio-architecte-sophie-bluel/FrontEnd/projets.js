187c830b4e6da05106bed3ebec844d1765427c4f
Nolwenn <bohelay.nolwenn@gmail.com>
2024-11-07 09:45:33 +0100

    build (boutons): coloration des boutons si actifs + fix code d'appel des catégories

--- Portfolio-architecte-sophie-bluel/FrontEnd/projets.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/projets.js
@@ -1,9 +1,19 @@
+// Récupération des projets
 const works = await fetch("http://localhost:5678/api/works")
-  .then(works => 
-    works.json())
+  .then(reponse => {
+    if (reponse.ok) {
+      return reponse.json();
+    }
+  })
+
+// Récupération des catégories
+const categories = await fetch("http://localhost:5678/api/categories")
+  .then(categories => 
+    categories.json())
 
 generateWorks(works);
 generateCategory(works);
+
 
   /* Ajout d'un nouveau projet pour tester */
 //   works.push(
@@ -22,61 +32,33 @@ generateCategory(works);
 
 function generateCategory(works) {
 
-  /**
-   * Qu'est ce qui peut être extrait de cette fonction
-   * 
-   * Premièrement, elle créé un array contenant les catégories (Peut-être mis dans une fonction à part)
-   * Ensuite, elle parcourt ces catégories pour crééer un bouton par catégorie.   
-   */
+  // Création bouton "Tous"
+  let btnTousElement = document.createElement ("div");
+  let btnTous = document.createElement("button");
+  btnTous.type = "button";
+  btnTous.setAttribute("class", "btnStyle");
 
-// Bouton "Tous"
-    let btnTousElement = document.createElement ("div");
-    let btnTous = document.createElement("button");
-    btnTous.type = "button";
-    btnTous.setAttribute("class", "btnStyle");
+  btnTous.innerText = "Tous";
+  btnTousElement.appendChild(btnTous);
+  document.querySelector("#divBoutons").appendChild(btnTousElement);
+
+  btnTous.onclick = () => {
+    let figures = document.querySelectorAll(".projet");
+    let afficherFigures = Array.from(figures);
+    afficherFigures.forEach (figure => figure.style.display = "block");
+    activeBtn(btnTous); //Appel fonction d'activation du bouton 
+  }
   
-    btnTous.innerText = "Tous";
-    btnTousElement.appendChild(btnTous);
-    document.querySelector("#divBoutons").appendChild(btnTousElement);
-
-    btnTous.onclick = () => {
-      let figures = document.querySelectorAll(".projet");
-      let afficherFigures = Array.from(figures);
-      afficherFigures.forEach (figure => figure.style.display = "block");
-    }
-// Autres boutons
-
-  //Création de 2 tableaux vides
-  let setCategorieUnique      = new Set(); //stockage catégories
-  let arrayCategoriesUniques  = []; //tableau sans doublons
-
-  // Récupération des catégories des projets
-  let worksCategory = []
-    works.forEach(projet => {
-      worksCategory.push(projet["category"])
-    })
-
-  // Pour chaque catégorie récupérée
-  worksCategory.forEach(categorie => {
-    // On vérifie que l'id de la catégorie n'est pas déjà présent dans le SET
-    if (!setCategorieUnique.has(categorie.id)) {
-      // S'il n'est pas présent dans le SET : On l'ajoute dans le SET
-      setCategorieUnique.add(categorie.id);
-      // Et on ajoute l'objet JSON categorie dans un array qui ne contient
-      // contient pas de doublons
-      arrayCategoriesUniques.push(categorie);
-    }
-  })
+// Création des autres boutons
 
   //boucle parcourant le nouveau tableau avec les 3 catégories
-  for (let i = 0; i < arrayCategoriesUniques.length; i++) {
+  for (let i = 0; i < categories.length; i++) {
     let categoryElements = document.createElement ("div");
     let categoryButton = document.createElement ("button");
 
     // Habillage des boutons (classes ou direct CSS)
-    /* Pour tes boutons, n'oublie pas d'ajouter un effet sur le bouton pour indiquer quel filtre est actif */
-    categoryButton.name = arrayCategoriesUniques[i]["name"];
-    categoryButton.setAttribute("data-id-categorie", arrayCategoriesUniques[i]["id"])
+    categoryButton.name = categories[i]["name"];
+    categoryButton.setAttribute("data-id-categorie", categories[i]["id"])
     categoryButton.type = "button";
     categoryButton.setAttribute("class", "btnStyle");
 
@@ -86,20 +68,24 @@ function generateCategory(works) {
     
     // Si l'attribut de la figure ne correspond pas à l'attribut du bouton, alors on cache le projet
     categoryButton.onclick = () => {
-        // On créé deux variables, l'une qui contient les élements à afficher et l'autre les élements à cacher
         let figuresACacher = Array.from(figures).filter(figure => figure.getAttribute("data-categorie") !== categoryButton.getAttribute("data-id-categorie"))
         let figuresAAfficher = Array.from(figures).filter(figure => figure.getAttribute("data-categorie") === categoryButton.getAttribute("data-id-categorie"))
         figuresACacher.forEach(figure => figure.style.display = "none");
         figuresAAfficher.forEach(figure => figure.style.display = "block");
+        categoryButton.classList.add("active");
+      
+        activeBtn(categoryButton);//Appel fonction d'activation du bouton 
       }
 
-    //Integration HTML
-    categoryButton.innerText = arrayCategoriesUniques[i].name;
-  
+  //Integration HTML
+    categoryButton.innerText = categories[i].name;
     categoryElements.appendChild(categoryButton);
-
-    // document.querySelectorAll(".gallery").innerHTML = categoryElements;
     document.querySelector("#divBoutons").appendChild(categoryElements);
+  }
+  // Fonction pour activation des boutons
+  function activeBtn (bouton){
+    document.querySelectorAll(".btnStyle").forEach(btn => btn.classList.remove("active"));
+    bouton.classList.add("active");
   }
 }
 
